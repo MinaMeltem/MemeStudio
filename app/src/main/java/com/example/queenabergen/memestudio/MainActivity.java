c1d70cfc114d4c3dabb0680512ac2b38a3340bca
qbergen <queenabergen@gmail.com>
2017-01-09 21:22:25 -0500

    Assemblying RecView and establishing staggered grid

--- app/src/main/java/com/example/queenabergen/memestudio/MainActivity.java
+++ app/src/main/java/com/example/queenabergen/memestudio/MainActivity.java
@@ -1,26 +1,50 @@
 package com.example.queenabergen.memestudio;
 
-import android.annotation.TargetApi;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
+import android.content.res.Resources;
+import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.widget.ImageView;
 
-import static com.example.queenabergen.memestudio.R.id.floatingActionButton;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @TargetApi(Build.VERSION_CODES.M)
+    private RecyclerViewAdapter mAdapter;
+    List<Integer> memePhotos = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.topToolbar);
-        FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(floatingActionButton);
-        int red = R.color.red;
-        floatingActionButton.setBackgroundColor(getColor(red));
+        memePhotos.add(R.drawable.meme);
+        memePhotos.add(R.drawable.meme);
+        memePhotos.add(R.drawable.meme);
+        memePhotos.add(R.drawable.meme);
+        memePhotos.add(R.drawable.meme);
+
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.topToolbar);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        Resources.Theme theme = getTheme();
+        ImageView topBannerPhoto = (ImageView) findViewById(R.id.logoBanner);
+        topBannerPhoto.setImageResource(R.drawable.meme);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+
+//        mAdapter = new RecyclerViewAdapter(memePhotos);
+        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager();
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/example/queenabergen/memestudio/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/queenabergen/memestudio/RecyclerViewAdapter.java
@@ -1,9 +1,54 @@
 package com.example.queenabergen.memestudio;
 
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
 /**
  * Created by queenabergen on 1/8/17.
  */
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>{
+    private List<Drawable> memePhotos;
+
+    public RecyclerViewAdapter(List<Drawable> photoList) {
+        this.memePhotos= photoList;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public CardView cardView;
+        public ImageView imageView;
+
+
+        public MyViewHolder(View view) {
+            super(view);
+            cardView = (CardView) view.findViewById(R.id.cardView);
+            imageView = (ImageView) view.findViewById(R.id.holderImageView);
+
+        }
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.viewholder, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.imageView.setImageDrawable(memePhotos.get(0));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
 }
