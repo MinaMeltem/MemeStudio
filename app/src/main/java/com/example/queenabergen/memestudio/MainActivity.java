680682c1d41951bf7b370b7cba9ea84c2d87fb65
qbergen <queenabergen@gmail.com>
2017-01-09 20:31:49 -0500

    Designed Rough ViewHolder

--- app/src/main/java/com/example/queenabergen/memestudio/MainActivity.java
+++ app/src/main/java/com/example/queenabergen/memestudio/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.queenabergen.memestudio;
 
+import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static com.example.queenabergen.memestudio.R.id.floatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.topToolbar);
+        FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(floatingActionButton);
+        int red = R.color.red;
+        floatingActionButton.setBackgroundColor(getColor(red));
     }
 }
---
+++ app/src/main/java/com/example/queenabergen/memestudio/RecyclerViewAdapter.java
@@ -0,0 +1,9 @@
+package com.example.queenabergen.memestudio;
+
+/**
+ * Created by queenabergen on 1/8/17.
+ */
+
+public class RecyclerViewAdapter {
+
+}
