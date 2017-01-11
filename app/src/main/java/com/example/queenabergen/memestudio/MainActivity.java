f9d77a61f439851a3bd325e42dbb9ab8529c306c
qbergen <queenabergen@gmail.com>
2017-01-11 16:09:37 -0500

    Update To Design, finishing RV, bug on start up using picasso library

--- app/src/main/java/com/example/queenabergen/memestudio/MainActivity.java
+++ app/src/main/java/com/example/queenabergen/memestudio/MainActivity.java
@@ -1,50 +1,57 @@
 package com.example.queenabergen.memestudio;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-
+    private Context context;
     private RecyclerViewAdapter mAdapter;
-    List<Integer> memePhotos = new ArrayList<>();
+    private RecyclerView.LayoutManager layoutManager;
+    private StaggeredGridLayoutManager staggeredGridLayoutManager;
 
 
+    private int[] memeOptions = {R.drawable.meme1,
+            R.drawable.meme2,
+            R.drawable.meme3,
+            R.drawable.meme4,
+            R.drawable.memes5,
+            R.drawable.meme6,
+            R.drawable.meme7,
+            R.drawable.meme8,
+            R.drawable.meme9,
+            R.drawable.meme10,
+            R.drawable.meme11,
+            R.drawable.meme12,
+            R.drawable.meme13,
+            R.drawable.meme14,
+            R.drawable.meme15,
+            R.drawable.meme16,
+            R.drawable.meme17,
+            R.drawable.meme18};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        memePhotos.add(R.drawable.meme);
-        memePhotos.add(R.drawable.meme);
-        memePhotos.add(R.drawable.meme);
-        memePhotos.add(R.drawable.meme);
-        memePhotos.add(R.drawable.meme);
 
 
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.topToolbar);
         FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         Resources.Theme theme = getTheme();
         ImageView topBannerPhoto = (ImageView) findViewById(R.id.logoBanner);
-        topBannerPhoto.setImageResource(R.drawable.meme);
+        topBannerPhoto.setImageResource(R.drawable.collapsebanner);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
-
-//        mAdapter = new RecyclerViewAdapter(memePhotos);
-        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager();
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(3, 1);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        mAdapter = new RecyclerViewAdapter(context, memeOptions );
         recyclerView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/example/queenabergen/memestudio/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/queenabergen/memestudio/RecyclerViewAdapter.java
@@ -1,38 +1,77 @@
 package com.example.queenabergen.memestudio;
 
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.util.List;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by queenabergen on 1/8/17.
  */
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>{
-    private List<Drawable> memePhotos;
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    private ImageView imageView;
 
-    public RecyclerViewAdapter(List<Drawable> photoList) {
-        this.memePhotos= photoList;
+    private int[] memeOptions = {R.drawable.meme1,
+            R.drawable.meme2,
+            R.drawable.meme3,
+            R.drawable.meme4,
+            R.drawable.memes5,
+            R.drawable.meme6,
+            R.drawable.meme7,
+            R.drawable.meme8,
+            R.drawable.meme9,
+            R.drawable.meme10,
+            R.drawable.meme11,
+            R.drawable.meme12,
+            R.drawable.meme13,
+            R.drawable.meme14,
+            R.drawable.meme15,
+            R.drawable.meme16,
+            R.drawable.meme17,
+            R.drawable.meme18};
+
+    public RecyclerViewAdapter(Context context , int[] memeOptions) {
+        getApplicationContext();
+        this.memeOptions = memeOptions;
+
     }
+
+    public Context getApplicationContext(){
+        return getApplicationContext();
+    }
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public CardView cardView;
         public ImageView imageView;
+        public TextView textView;
 
 
-        public MyViewHolder(View view) {
-            super(view);
-            cardView = (CardView) view.findViewById(R.id.cardView);
-            imageView = (ImageView) view.findViewById(R.id.holderImageView);
+        public MyViewHolder(View itemview) {
+            super(itemview);
+            cardView = (CardView) itemview.findViewById(R.id.cardView);
+            imageView = (ImageView) itemview.findViewById(R.id.holderImageView);
+            itemview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    Snackbar.make(v, "Making Sure It Works" + position,
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
 
         }
     }
+
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
@@ -42,13 +81,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.imageView.setImageDrawable(memePhotos.get(0));
+    public void onBindViewHolder(MyViewHolder holder, int i) {
+
+        holder.imageView.setImageResource(memeOptions[i]);
+        Picasso.with(getApplicationContext()).load(memeOptions[i]).into(imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return memeOptions.length;
     }
 
 }
